52e3f91cebb7a301234c9b1105fa8dd3e7531e3a
Matt Vogel <mainmoniker@googlemail.com>
2023-10-11 20:50:08 -0400

    feat: 721

--- quartz/components/Body.tsx
+++ quartz/components/Body.tsx
@@ -1,13 +1,18 @@
 // @ts-ignore
 import clipboardScript from "./scripts/clipboard.inline"
 import clipboardStyle from "./styles/clipboard.scss"
+import carouselScript from "./scripts/carouselVersion.inline"
 import { QuartzComponentConstructor, QuartzComponentProps } from "./types"
 
 function Body({ children }: QuartzComponentProps) {
   return <div id="quartz-body">{children}</div>
 }
 
-Body.afterDOMLoaded = clipboardScript
+Body.afterDOMLoaded = () => {
+  clipboardScript();
+  carouselScript();
+}
+
 Body.css = clipboardStyle
 
 export default (() => Body) satisfies QuartzComponentConstructor
--- quartz/components/pages/404.tsx
+++ quartz/components/pages/404.tsx
@@ -3,10 +3,15 @@ import { QuartzComponentConstructor } from "../types"
 function NotFound() {
   return (
     <article class="popover-hint">
-      <h1>404</h1>
-      <p>Either this page is private or doesn't exist.</p>
+      <h1 style="font-size:40px; font-family: 'Cinzel', sans-serif !important;">Error 721: Lost in the Weeds</h1>
+      <p style="font-family: 'Italiana', sans-serif !important;">Looks like you've ventured further than I've pruned!</p>
+      <p style="font-family: 'Italiana', sans-serif !important;">Let's get you back on the <a href="https://8bitgentleman.github.io/matt/">garden path</a></p>
+
     </article>
   )
 }
-
+// font-family: 'Alegreya', serif;
+// font-family: 'Cinzel', serif;
+// font-family: 'Lora', serif;
+// font-family: 'Poiret One', sans-serif;
 export default (() => NotFound) satisfies QuartzComponentConstructor
